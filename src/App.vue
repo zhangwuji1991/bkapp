e3580d4b7b0b6f35d44c73ee5d71646eaeb95dc5
zhangwuji <191933284@qq.com>
2018-03-29 16:02:13 +0800

    frist

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <Foot/>
+  </div>
+</template>
+
+<script>
+import Foot from '@/components/foot'
+export default {
+  name: 'App',
+  components: {
+    Foot
+  },
+  mounted () {
+      this.appShow()
+    },
+    methods: {
+      appShow () {
+        const { PAGE_START_TIME } = window
+        const END_TIME = new Date().getTime() // 结束时间
+        const diffTime = END_TIME - PAGE_START_TIME
+        const timer = setTimeout(() => {
+          clearTimeout(timer)
+          document.querySelector('.app-loading').className += ' app-loading-hide'
+        }, diffTime > 2000 ? 0 : 2000 - diffTime)
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+   <div class="heade">
+     个人中心
+   </div>
+   <nav class="nav">
+        <li>
+          <router-link :to="{ name: 'topic-create' }" flex="box:justify">
+            <div class="icon" flex="cross:center">
+              <i class="iconfont icon-edit" style="color: #14b11d"></i>
+            </div>
+            <div class="text">发表主题</div>
+            <div class="arrow">
+              <i class="iconfont icon-arrow-right"></i>
+            </div>
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'self-messages' }" flex="box:justify">
+            <div class="icon" flex="cross:center">
+              <i class="iconfont icon-msg" style="color: #bd1616;"></i>
+            </div>
+            <div class="text">我的消息</div>
+            <div class="arrow">
+              <i class="iconfont icon-arrow-right"></i>
+            </div>
+          </router-link>
+        </li>
+      </nav>
+  </div>
+</template>
+<script>
+  // import { mapModules } from 'vuet'
+
+  // export default {
+  //   mixins: [
+  //     mapModules({ self: 'user-self' })
+  //   ]
+  // }
+
+</script>
+<style lang="less" scoped>
+ @import "../less/config";
+ .heade{
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    height: 50px;
+    line-height: 50px;
+    border: 1px solid #ddd;
+ }
+  .nav {
+    padding: 0;
+    margin-top:  60px;
+    li {
+      list-style: none;
+      border-bottom: 1px solid #ddd;
+      background: #fbfbfb;
+      &:first-of-type {
+        border-top: 1px solid #ddd;
+      }
+    }
+    a {
+      padding: 8px 10px;
+      line-height: 28px;
+      .icon {
+        width: 45px;
+        .iconfont {
+          font-size: 24px;
+          color: #aaa;
+        }
+      }
+      .text {
+        line-height: 30px;
+        font-size: 13px;
+        color: @text;
+      }
+      .icon-arrow-right {
+        color: #ccc;
+      }
+    }
+  }
+</style>
